f505445dae273a44c4d758a2c6def56addaf3510
kathiravan C <124439980+Kathiravan292@users.noreply.github.com>
2025-07-26 12:20:31 +0530

    Add files via upload

---
+++ form.js
@@ -0,0 +1,10 @@
+let pa = document.getElementById("pswd");
+let btn = document.getElementById("btn");
+let image = document.getElementById("bgimage")
+
+pa.addEventListener('input',()=>{
+    let pass = pa.value;
+      let length = pass.length
+      let val = 20 - length * 2
+      bgimage.style.filter = `blur(${val}px)`
+});
